6e20d8ec5c3f5d15eb7532d1631995782e668604
Lars Wetendorff Nielsen <larswn@me.com>
2026-06-24 20:52:48 +0200

    feat: add fullName field to User model and create migration script

---
+++ prisma/migrations/20260624185214_add_user_full_name_field/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `fullName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("id", "password", "role", "username") SELECT "id", "password", "role", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
