4d5e8f36528a2d7d37aed6b397943b3cbfd8a5c9
Lars Wetendorff Nielsen <larswn@me.com>
2026-06-28 15:50:10 +0200

    refactor: remove unnecessary string from TextDivider component for cleaner markup

--- components/TextDivider.tsx
+++ components/TextDivider.tsx
@@ -5,9 +5,9 @@ type TextDividerProps = {
 export default function TextDivider({ text }: TextDividerProps) {
 	return (
 		<div className="my-8 flex justify-between items-center gap-4">
-			<div className="bg-border h-px grow">{""}</div>
+			<div className="bg-border h-px grow"></div>
 			<div className="shrink text-xs text-muted">{text}</div>
-			<div className="bg-border h-px grow">{""}</div>
+			<div className="bg-border h-px grow"></div>
 		</div>
 	);
 }
